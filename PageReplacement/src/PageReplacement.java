be844cfb32b28fab372c23cfcffe2fef48997192
AgentBosco <rohan111maniyar@gmail.com>
2020-04-22 10:35:54 +0530

    Add files via upload

---
+++ PageReplacement/src/PageReplacement.java
@@ -0,0 +1,14 @@
+public class PageReplacement 
+{
+	public static void main(String[] args) 
+	{	
+		FCFS fcfs=new FCFS();
+		fcfs.execute();
+		
+		LRU lru=new LRU();
+		lru.execute();
+		
+		Optimal optimal=new Optimal();
+		optimal.execute();
+	}
+}
